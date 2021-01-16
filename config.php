d054583a134a18f6f1d76cc959558f5802803efc
Nandini <52810541+Nandini2510@users.noreply.github.com>
2021-01-16 14:48:40 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('ROOT_URL','http://localhost:8080/Bank_system');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','nand25');
+define('DB_NAME','bank_user');
+
+?>
